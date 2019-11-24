05966441b8330a1debc50c1176bfcdf802d5c2c9
JieningLiSS <jiening.li@smoothstack.com>
2019-11-23 23:21:52 -0500

    branch commit

--- Demo/src/Test.java
+++ Demo/src/Test.java
@@ -3,8 +3,8 @@ public abstract class Test{
 
   public static void main(String[] args) {
 
-		System.out.println("Most recent !");
-		System.out.println("This is my init commit!");
+
+	  System.out.println("branch commit!");
 
 	}
 
