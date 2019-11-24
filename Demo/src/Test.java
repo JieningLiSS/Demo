9aee5aa34e4bc1f37052fac60ec5ca0f23887ada
JieningLiSS <55749974+JieningLiSS@users.noreply.github.com>
2019-11-23 23:51:54 -0500

    Update Test.java

--- Demo/src/Test.java
+++ Demo/src/Test.java
@@ -1,17 +1,6 @@
 
 public abstract class Test{
-
   public static void main(String[] args) {
-
-<<<<<<< HEAD
-
-	  System.out.println("branch commit!");
-=======
-	    System.out.println("new change");
-	
-
->>>>>>> master
-
-
+System.out.println("master");
 	}
 }
