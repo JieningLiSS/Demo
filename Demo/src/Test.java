4d3cf6c13f6348d5d8ab9d32a963f78a8b8b25a6
JieningLiSS <55749974+JieningLiSS@users.noreply.github.com>
2019-11-23 23:18:02 -0500

    Update Test.java

--- Demo/src/Test.java
+++ Demo/src/Test.java
@@ -3,12 +3,10 @@ public abstract class Test{
 
   public static void main(String[] args) {
 
-		System.out.println("Most recent !");
-		System.out.println("This is my init commit!");
-	        System.out.println("New commit!");
+	    System.out.println("new change");
+	
 
 
-    System.out.println("This is commit from my branch!");
 
 	}
 }
